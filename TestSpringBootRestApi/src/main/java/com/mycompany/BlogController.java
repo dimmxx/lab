90e122f5f88ff8a505ef96c687882aea7857f65c
dimmxx <dimmxx@gmail.com>
2019-12-04 18:57:29 +0200

    linux work:

--- TestSpringBootRestApi/src/main/java/com/mycompany/BlogController.java
+++ TestSpringBootRestApi/src/main/java/com/mycompany/BlogController.java
@@ -1,7 +1,14 @@
 package com.mycompany;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +21,19 @@ public class BlogController {
     public String indexStart() {
         return "Congratulations from BlogController.java";
     }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Object> uploadFile(@RequestParam("file")MultipartFile file, @RequestParam("type") String type) throws IOException {
+        File convertFile = new File("src/main/resources/" + file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
+        fileOutputStream.write(file.getBytes());
+        fileOutputStream.close();
+        return new ResponseEntity<>("File uploaded succesfully", HttpStatus.OK);
+    }
+
+
+
 
     @GetMapping("/blog")
     public List<Blog> index() {
