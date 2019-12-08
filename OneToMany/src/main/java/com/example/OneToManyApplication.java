e7aed0da7b3869b91f3327bd096f954a4cf7ac27
dimmxx <dimmxx@gmail.com>
2019-12-08 21:21:29 +0200

    onetomany test

---
+++ OneToMany/src/main/java/com/example/OneToManyApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyApplication {
+
+	//https://www.callicoder.com/spring-boot-rest-api-tutorial-with-mysql-jpa-hibernate/
+	public static void main(String[] args) {
+		SpringApplication.run(OneToManyApplication.class, args);
+	}
+
+}
