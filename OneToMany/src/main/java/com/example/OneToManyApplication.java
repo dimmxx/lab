61d2fcd0fea4d4f49af7eef0de8f32afe554858c
dimmxx <dimmxx@gmail.com>
2019-12-09 22:19:19 +0200

    home mac

--- OneToMany/src/main/java/com/example/OneToManyApplication.java
+++ OneToMany/src/main/java/com/example/OneToManyApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OneToManyApplication {
 
 	//https://www.callicoder.com/spring-boot-rest-api-tutorial-with-mysql-jpa-hibernate/
